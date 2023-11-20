28336432436a532770de0c19022f5ce62d2b2004
luis rodriguez <lucol6491[200~>
2023-11-20 10:20:30 -0500

    base del codigo del carrusel

--- scrips.js
+++ scrips.js
@@ -1,15 +1,53 @@
-const $carrusel = document.querySelector(".carrusel");
-const $animacion = document.querySelectorAll(".producto1");
-$carrusel.addEventListener("mouseenter", () => {
-  console.log($animacion);
-  for (i of $animacion) {
-    i.classList.add("stop");
+const $carrusel = document.querySelectorAll(".carrusel-imagenes img");
+const $cardVideos = document.querySelectorAll(".chimuzero");
+let i = 0;
+let j = 0;
+function siguente(elementos) {
+  if (i === 2) {
+    elementos[i].classList.remove("activa");
+    i = 0;
+    elementos[i].classList.add("activa");
+  } else {
+    elementos[i].classList.remove("activa");
+    elementos[i + 1].classList.add("activa");
+    i++;
   }
-});
+}
+setInterval(() => {
+  if (j === 2) {
+    $cardVideos[j].classList.remove("activa");
+    j = 0;
+    $cardVideos[j].classList.add("activa");
+  } else {
+    $cardVideos[j].classList.remove("activa");
+    $cardVideos[j + 1].classList.add("activa");
+    j++;
+  }
+}, 6000);
 
-$carrusel.addEventListener("mouseleave", () => {
-  console.log($animacion);
-  for (i of $animacion) {
-    i.classList.remove("stop");
+setInterval(() => {
+  siguente($carrusel);
+}, 6000);
+//carrusel videos
+/*
+setInterval(() => {
+  if (j === 2) {
+    $carruselVideos[j].classList.remove("ver-video");
+    //pocicion
+    $p[j].classList.remove("video-activo");
+
+    j = 0;
+    $carruselVideos[j].classList.add("ver-video");
+    //pocicion
+    $p[j].classList.add("video-activo");
+  } else {
+    $carruselVideos[j].classList.remove("ver-video");
+    $carruselVideos[j + 1].classList.add("ver-video");
+    //pocicion
+    $p[j].classList.remove("video-activo");
+    $p[j + 1].classList.add("video-activo");
+
+    j++;
   }
-});
+}, 5000);
+*/
